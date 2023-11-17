13e24b20e7c6b737fd2001d6c5e2aed92e77dad2
ConradoEC <113607331+ConradoEC@users.noreply.github.com>
2023-11-17 10:28:51 -0300

    fixing the 'Access-Control-Allow-Origin', fourth try

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ app.use(routes)
 
 app.use((req, res, next) => 
 {
-  app.use(cors());
-  res.header("Access-Control-Allow-Origin", '*');
-  next();
+  console.log('ola')
+  // app.use(cors());
+  // res.header("Access-Control-Allow-Origin", "*");
+  // next();
 });
 
 app.listen(process.env.PORT ? Number(process.env.PORT) : PORT, () => 
