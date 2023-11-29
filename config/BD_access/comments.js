b0a61be618cb48ac9271b37a897c433b95963481
ConradoEC <113607331+ConradoEC@users.noreply.github.com>
2023-11-29 04:52:02 -0300

    Updates

--- config/BD_access/comments.js
+++ config/BD_access/comments.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2')
 
 const comments_connection = mysql.createConnection({
-    host: '85.10.205.173',
-    user: 'grupo_a_2ds',
-    password: 'site_de_medicina',
-    database: 'inter_2ds'
+    host: 'sql10.freesqldatabase.com',
+    user: 'sql10665993',
+    password: 'gyD43pWnva',
+    database: 'sql10665993'
 })
 
 module.exports = comments_connection
