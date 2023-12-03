edcdf4a7ae4926277b7cc2622fe26cd8b9de84a2
ConradoEC <113607331+ConradoEC@users.noreply.github.com>
2023-12-03 13:32:28 -0300

    Updating to localhost DB

--- config/BD_access/dbvaccine.js
+++ config/BD_access/dbvaccine.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const dbvaccine = mysql.createConnection({
-    host: 'sql10.freesqldatabase.com',
-    user: 'sql10665993',
-    password: 'gyD43pWnva',
-    database: 'sql10665993'
+    host: 'localhost',
+    user: 'root',
+    password: '123321',
+    database: 'vaccines'
 })
 
 // host: '85.10.205.173',
